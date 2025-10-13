98848c1ba16272c3148bb7ec981c3e00724d7fa1
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2025-10-13 13:14:53 +0530

    Update auth.controller.js

--- src/modules/auth/controllers/auth.controller.js
+++ src/modules/auth/controllers/auth.controller.js
@@ -15,11 +15,14 @@ const { autoVerifyID } = require("../../../utils/VerifyAuto"); // optional (Goog
 // =============================
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
-}
-// =============================
+
+  console.log("✅ Firebase Admin Initialized Successfully");
+}// =============================
 // 🔐 Helper: Generate JWT
 // =============================
 const generateToken = (id) =>
