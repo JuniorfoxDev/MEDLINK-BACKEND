7c20ee9bf0863cdc05f7eb015eb0e2fc28f818c1
Vaibhav <juniorfox2908@gmail.com>
2025-10-13 12:15:45 +0530

    original

--- src/modules/auth/controllers/auth.controller.js
+++ src/modules/auth/controllers/auth.controller.js
@@ -14,7 +14,7 @@ const { autoVerifyID } = require("../../../utils/VerifyAuto"); // optional (Goog
 // 🔥 FIREBASE SETUP
 // =============================
 if (!admin.apps.length) {
-  const serviceAccount = require("../../../config/firebaseServiceKey.json");
+  const firebaseConfig = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
