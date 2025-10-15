101bb5309cf33817cf7b867812a22227ec4626da
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2025-10-15 00:45:34 +0000

    done

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,11 +1,19 @@
 const admin = require("firebase-admin");
 
-const credentials = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
-
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(credentials),
-  });
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      }),
+    });
+
+    console.log("🔥 Firebase initialized successfully!");
+  } catch (err) {
+    console.error("❌ Firebase initialization failed:", err.message);
+  }
 }
 
 module.exports = admin;
