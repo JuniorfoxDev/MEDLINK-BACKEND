4fd76765e56e263319284f3fa023d6e8db577255
Vaibhav <76144239+JuniorfoxDev@users.noreply.github.com>
2025-10-15 03:19:37 +0000

    sy

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "https://medlink-frontend.vercel.app",
   "https://medlink-frontend-4t49.vercel.app/",
   "https://medlink-production.vercel.app",
+  "https://medlink-frontend-4t49-quxbj2kmd-juniorfoxdevs-projects.vercel.app",
   "https://medlink-prod.vercel.app",
   "https://medlink-frontend-4t49-307czvbwn-juniorfoxdevs-projects.vercel.app", // your actual deployed frontend
 ];
